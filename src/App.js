f6c583bc135e7cb0efdfd9e2667779cfc7f1986b
Dmytro Doroshkevych <ddoroshkevych@hotmail.com>
2022-07-21 13:55:07 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const INTERVAL = 10;
+
+  const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(INTERVAL);
+  const [isGameRunning, setIsGameRunning] = useState(false);
+  const [numWords, setNumWords] = useState(0);
+
+  useEffect(() => {
+    if (isGameRunning === true && timeRemaining > 0) {
+      setTimeout(() => setTimeRemaining((prevTime) => prevTime - 1), 1000);
+    } else if (timeRemaining === 0) {
+      setIsGameRunning(false);
+      setNumWords(countWords(text));
+    }
+  }, [timeRemaining, isGameRunning]);
+
+  function changeText(event) {
+    setText(event.target.value);
+  }
+
+  function countWords(text) {
+    const wordsArray = text
+      .trim()
+      .split(" ")
+      .filter((el) => el !== "");
+    return wordsArray.length;
+  }
+
+  function startGame() {
+    setIsGameRunning(true);
+    setNumWords(0);
+    setTimeRemaining(INTERVAL);
+    setText("");
+  }
+
+  return (
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea
+        disabled={!isGameRunning}
+        value={text}
+        name="textarea"
+        onChange={changeText}
+      />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button disabled={isGameRunning} onClick={startGame}>
+        Start
+      </button>
+      <h1>Word count: {numWords}</h1>
+    </div>
+  );
+}
+
+export default App;
